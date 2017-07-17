3d53379d1d1e197289b64ffe3d12aac51fe357aa
Terry Cain <terry@terrys-home.co.uk>
2017-07-17 23:08:23 +0100

    Fixed rst formatting

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ with open('README.rst') as readme_file:
     readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+    history = ''.join(history_file.readlines()[1:])
+
 
 requirements = [
     'aiohttp',
