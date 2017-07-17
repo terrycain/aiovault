bd9b040422b3260e28a3074b6e9251ecfe9469ea
Terry Cain <terry@terrys-home.co.uk>
2017-07-17 23:08:45 +0100

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ test_requirements = [
 
 setup(
     name='aiovault',
-    version='0.1.2',
+    version='0.1.3',
     description="Vault asyncio",
     long_description=readme + '\n\n' + history,
     author="Terry Cain",
